1ec83fd6029763f9d3ec61b12772767378648b25
Dylan Simon <grvdylans@gmail.com>
2025-12-24 14:51:52 -0700

    File uploader is now connected to S3 - unfinished

--- controllers/filesController.js
+++ controllers/filesController.js
@@ -1,4 +1,30 @@
-async function test(req, res) {
+import {
+    S3Client,
+    PutObjectCommand,
+    CreateBucketCommand,
+    DeleteObjectCommand,
+    DeleteBucketCommand,
+    paginateListObjectsV2,
+    GetObjectCommand,
+} from "@aws-sdk/client-s3";
+
+const s3client = new S3Client({ region: "us-east-2" });
+
+async function checkCredentials(req, res) {
+    res.send("hi");
+}
+
+async function createBucket(req, res) {
+    const bucketName = "ldg-guides-images";
+    const result = await s3client.send(
+        new CreateBucketCommand({
+            Bucket: bucketName,
+        }),
+    );
+    res.send(result);
+}
+
+async function uploadFile(req, res) {
     console.log({
         title: req.file.originalname,
         url: req.file.path,
@@ -13,4 +39,9 @@ async function test(req, res) {
     });
 }
 
-export default { test };
+async function test(req, res) {
+    console.log("received file get request");
+    res.send({ message: "hi8" });
+}
+
+export default { test, createBucket, uploadFile, checkCredentials };
--- routes/filesRouter.js
+++ routes/filesRouter.js
@@ -1,10 +1,33 @@
 import { Router } from "express";
-const router = Router();
+import {
+    S3Client,
+    PutObjectCommand,
+    CreateBucketCommand,
+    DeleteObjectCommand,
+    DeleteBucketCommand,
+    paginateListObjectsV2,
+    GetObjectCommand,
+} from "@aws-sdk/client-s3";
 import multer from "multer";
+import multerS3 from "multer-s3";
 import filesController from "../controllers/filesController.js";
 
-const upload = multer({ dest: "uploads/" });
+const s3client = new S3Client({ region: "us-east-2" });
+const router = Router();
 
-router.post("/", upload.single("upload-file"), filesController.test);
+const upload = multer({
+    storage: multerS3({
+        s3: s3client,
+        bucket: "ldg-guides-images",
+        metadata: function (req, file, cb) {
+            cb(null, { fieldName: file.fieldname });
+        },
+        key: function (req, file, cb) {
+            cb(null, Date.now().toString());
+        },
+    }),
+});
+router.get("/", filesController.createBucket);
+router.post("/", upload.single("upload-file"), filesController.uploadFile);
 
 export default router;
