7a4b94d4cfe30c856f74272c07e6feed47931ab7
cloudwolf <206946888+xerxes324@users.noreply.github.com.>
2026-02-06 19:48:14 +0530

    add rename, reorder and navbar routes

--- routes/sectionsRouter.js
+++ routes/sectionsRouter.js
@@ -1,10 +1,13 @@
 import requireAdmin from "../config/requireAdmin.js";
-import createSection from "../controllers/sectionsController.js";
+import createSection, { renameSection, reorderSection, getNavbar } from "../controllers/sectionsController.js";
 import { deleteSection } from "../controllers/sectionsController.js";
 import { Router } from "express";
 const router = Router();
 
 router.post("/", requireAdmin, createSection)
 router.delete("/delete/:id", requireAdmin, deleteSection)
+router.put("/rename/:id", requireAdmin, renameSection)
+router.put("/reorder", requireAdmin, reorderSection)
+router.get("/navbar", getNavbar)
 
 export default router;
